9a5f536fb9a936462eb95a13b595e1f1e81c26d8
Tenyson05 <rojahog@Hotmail.com>
2023-09-15 13:05:33 -0500

    Customize files to match personal needs

---
+++ scripts/sslscan.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -Eeuo pipefail
+trap "echo -e \"\033[1;31m[!] \e[0m Script error occured.\"" ERR
+
+GREEN="\033[1;32m"
+ENDCOLOR="\e[0m"
+
+# === Exit without proceeding if run in WSL ===
+if [ -f /mnt/c/Windows/System32/wsl.exe ]; then
+    exit 0
+fi
+
+# === Exit without proceeding if run on a non-Kali distro ===
+if ! grep -Fxvq "kali-rolling" /etc/debian_version; then
+    exit 0
+fi
+
+# Check if sslscan is installed
+if [ ! -d "/opt/sslscan" ]
+then
+    echo -ne $GREEN"[+] "$ENDCOLOR; echo "Installing sslscan"
+    sudo apt install sslscan
+fi
